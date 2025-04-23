28a7f05c3915f8e8685a843ee3c2c283435fe30d
Amit Naik <amitnaikkwr@gmail.com>
2025-04-23 14:25:05 +0530

    allocate slot

--- src/parking/parking.controller.ts
+++ src/parking/parking.controller.ts
@@ -1,17 +1,23 @@
 import { Controller, Post, Body, Patch } from '@nestjs/common';
 import { ParkingService } from './parking.service';
 
-@Controller('parking_lot')
+@Controller()
 export class ParkingController {
     constructor(private readonly parkingService: ParkingService) {};
 
-    @Post()
+    @Post('parking_lot')
     initialize(@Body('no_of_slot') noOfSlots: number) {
         return this.parkingService.initializeParkingLot(noOfSlots);
     }
 
-    @Patch()
+    @Patch('parking_lot')
     expand(@Body('increment_slot') increment: number) {
         return this.parkingService.expandParkingLot(increment);
     } 
+
+    @Post('park') 
+    park(@Body() body: { car_reg_no: string; car_color: string}) {
+        const { car_reg_no, car_color} = body;
+        return this.parkingService.allocateParkingSlot(car_reg_no, car_color);
+    }
 }
--- src/parking/parking.service.ts
+++ src/parking/parking.service.ts
@@ -8,7 +8,7 @@ interface Car {
 @Injectable()
 export class ParkingService {
     private slots: (Car | null)[] = [];
-    private colorToRegSlot = new Map<string, Set<{regNo: string, slot: string}>>();
+    private colorToRegSlot = new Map<string, Set<{regNo: string, slot: number}>>();
 
     initializeParkingLot(noOfSlots: number) : {total_slot: number} {
         if(this.slots.length > 0) {
@@ -28,5 +28,24 @@ export class ParkingService {
         }
 
         return { total_slot: this.slots.length}
+    }
+
+    allocateParkingSlot(carRegNo: string, carColor: string): { allocated_slot_number: number} {
+        const availableSlotIndex = this.slots.findIndex(slot => slot === null);
+
+        if (availableSlotIndex === -1) {
+            throw new Error('Parking lot is full. You have been added to waitlist');
+        }
+
+        const allocatedSlotNumber = availableSlotIndex + 1;
+        const newCar: Car = { regNo: carRegNo, color: carColor};
+        this.slots[availableSlotIndex] = newCar;
+
+        if(!this.colorToRegSlot.has(carColor)) {
+            this.colorToRegSlot.set(carColor, new Set());
+        }
+        this.colorToRegSlot.get(carColor)?.add({regNo: carRegNo, slot: allocatedSlotNumber});
+
+        return { allocated_slot_number : allocatedSlotNumber};
     }
 }
